6a39a1a22791372051e94033ebae47cc3d8867b4
AgungR12 <2000agungramadhan@gmail.com>
2023-10-26 17:48:22 +0700

    xendit_payment_callback

---
+++ src/api/order/helpers/header.js
@@ -0,0 +1,13 @@
+// untuk generate token dan export untuk send with header
+require('dotenv').config(); // menghubungkan ke file env
+// function generateToken
+const generateToken = () => {
+    const xendit_api_key = process.env.XENDIT_KEY+':';
+    const base64 = Buffer.from(xendit_api_key).toString('base64');
+    return base64;
+}
+
+exports.xenditHeaders = {
+    'Authorization' : 'Basic ' +generateToken(),
+    'Content-Type' : 'application/json'
+}
